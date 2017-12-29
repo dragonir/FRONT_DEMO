fed83dd377a37edbc474c8296c4f123fee6aa341
dragonir <dragonir@sina.cn>
2017-12-29 23:40:13 +0800

    Initial commit

---
+++ 03. 页面特效/jQuery Scrollify 一款鼠标滚轮控制页面滚动效/js/main.js
@@ -0,0 +1,11 @@
+/*素材家园 - www.sucaijiayuan.com*/
+$(function() {
+	$(".panel").css({"height":$(window).height()});
+	$.scrollify({
+		section:".panel"
+	});
+	$(".scroll").click(function(e) {
+		e.preventDefault();
+		$.scrollify("move",$(this).attr("href"));
+	});
+});
